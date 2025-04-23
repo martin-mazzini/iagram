09c3c4f3295e12916c3a02bcf1884b1924a6054e
Martín Mazzini <mjuliom_4@hotmail.com>
2025-04-22 23:45:58 -0300

    fix dns config

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -1,22 +1,22 @@
-# Route53 configuration for iagram.net
-resource "aws_route53_zone" "main" {
-  name = "iagram.net"
+data "aws_route53_zone" "main" {
+  name         = "iagram.net."
+  private_zone = false
 }
 
 # A record pointing to the Elastic IP
 resource "aws_route53_record" "www" {
-  zone_id = aws_route53_zone.main.zone_id
+  zone_id = data.aws_route53_zone.main.zone_id
   name    = "www.iagram.net"
   type    = "A"
-  ttl     = "300"
+  ttl     = 300
   records = [module.ec2.elastic_ip]
 }
 
 # A record for the root domain
 resource "aws_route53_record" "root" {
-  zone_id = aws_route53_zone.main.zone_id
+  zone_id = data.aws_route53_zone.main.zone_id
   name    = "iagram.net"
   type    = "A"
-  ttl     = "300"
+  ttl     = 300
   records = [module.ec2.elastic_ip]
 } 
